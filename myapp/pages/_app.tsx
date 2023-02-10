7893445de209d596859e4c894cfa8827087678d7
AbdelhaqMAHMOUDI <abdelhak572@gmail.com>
2023-02-10 10:03:19 +0100

    page de connexion

--- myapp/pages/_app.tsx
+++ myapp/pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import { Route } from 'react-router-dom'
 import Home from '.'
 import Page2 from './inscription_ok'
-
+import Login from './login'
+import Admin from './admin'
 export default function App({ Component, pageProps }: AppProps) {
 
   <><Route path="/" children={<Home />} />
-    <Route path="/inscription_ok" children={<Page2 />} /></>
+    <Route path="/login" children={<Login />} />
+    <Route path="/inscription_ok" children={<Page2 />} />
+    <Route path="/admin" children={<Admin />} /></>
 
   return <Component {...pageProps} />
 }
